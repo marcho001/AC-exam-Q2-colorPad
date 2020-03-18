685c588e3174c3e82e4a72d2b496bfc9d03bac3d
Marc <g40419@gmail.com>
2020-03-18 16:09:19 +0800

    all complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# RGB to HEX converter
+
+### 使用range 調整RGB數值 可在背景得到顏色和HEX的值
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const hexVal = document.querySelector('#hex-value')
+const background = document.querySelector('#background')
+let red = '00'
+let green = '00'
+let blue = '00'
+
+
 function showVal(newVal) {
   let results = newVal.id
+  let value = Number(newVal.value).toString(16)
+
+  if(value.length < 2){
+    value = '0' + value
+  }
+
   switch(results){
     case 'red':
+      red = value
+      hexVal.innerText = `#${red}${green}${blue}`
       document.querySelector('#red-results').innerHTML = `<h4>${newVal.value}</h4>`;
       break;
     case 'green':
+      green = value
+      hexVal.innerText = `#${red}${green}${blue}`
       document.querySelector('#green-results').innerHTML = `<h4>${newVal.value}</h4>`;
       break;
     case 'blue':
+      blue = value
+      hexVal.innerText = `#${red}${green}${blue}`
       document.querySelector('#blue-results').innerHTML = `<h4>${newVal.value}</h4>`;
   }
+  background.style.backgroundColor = `#${red}${green}${blue}` 
 }
+
